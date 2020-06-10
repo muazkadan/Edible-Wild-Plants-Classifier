54d88aa748513cdac31da731d85f29a50275204f
Mouaz KAADAN <mouaz.kaadan@gmail.com>
2020-06-10 16:44:22 +0300

    Second commit

--- Python/work1.py
+++ Python/work1.py
@@ -5,11 +5,15 @@ import matplotlib.pyplot as plt
 import os
 import cv2
 import random as rnd
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPool2D
+from tensorflow.keras.utils import normalize
+import numpy as np
 
 print("Tensorflow version : " + tf.__version__) #2.2.0
 
-IMG_WIDTH = int(1920 / 10)
-IMG_HEIGHT = int(1080 / 10)
+IMG_HEIGHT  = int(1920 / 10)
+IMG_WIDTH = int(1080 / 10)
 DataDir = "/home/mouaz/PycharmProjects/Bitirme/dataset"
 CATEGORIES = ["Alfalfa", "Asparagus", "Blue Vervain", "Broadleaf Plantain", "Bull Thistle", "Cattail", "Chickweed",
               "Chicory", "Cleavers", "Coltsfoot", "Common Sow Thistle", "Common Yarrow", "Coneflower",
@@ -33,8 +37,8 @@ for category in CATEGORIES:
     path = os.path.join(DataDir, category)
     class_num = CATEGORIES.index(category)
     for img in os.listdir(path):
-        img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_UNCHANGED)
-        new_array = cv2.resize(img_array, (IMG_HEIGHT, IMG_WIDTH))
+        img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_COLOR )
+        new_array = cv2.resize(img_array, (IMG_WIDTH, IMG_HEIGHT))
         training_data.append([new_array, class_num])
 
 plt.imshow(training_data[1][0])
@@ -43,3 +47,37 @@ plt.show()
 print(len(training_data))
 
 rnd.shuffle(training_data)
+
+x = []
+y = []
+
+for features, label in training_data:
+    x.append(features)
+    y.append(label)
+
+x = np.array(x).reshape(-1, IMG_HEIGHT, IMG_WIDTH, 3)
+
+X = np.array(normalize(x))
+y = np.array(y)
+
+model = Sequential()
+model.add(Conv2D(64, (3,3), input_shape = X.shape[1:]))
+model.add(Activation("relu"))
+model.add(MaxPool2D(pool_size=(2,2)))
+
+model.add(Conv2D(64, (3,3)))
+model.add(Activation("relu"))
+model.add(MaxPool2D(pool_size=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+
+model.add(Dense(3))
+model.add(Activation("sigmoid"))
+
+model.compile(loss="categorical_crossentropy",
+              optimizer="adam",
+              metrics=["accuracy"])
+
+model.fit(X, y, batch_size=32)
+
