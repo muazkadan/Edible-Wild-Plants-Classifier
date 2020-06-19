2e381a5d3402e5661be662c28ba31b93c37d2901
Mouaz KAADAN <mouaz.kaadan@gmail.com>
2020-06-19 17:09:17 +0300

    OpenCV Commit

--- Android/app/src/main/java/net/mouazkaadan/seniordesignprojecti/MainActivity.java
+++ Android/app/src/main/java/net/mouazkaadan/seniordesignprojecti/MainActivity.java
@@ -3,6 +3,9 @@ package net.mouazkaadan.seniordesignprojecti;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(OpenCVLoader.initDebug())
+            Toast.makeText(MainActivity.this, "OpenCV Loaded Successfully", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(MainActivity.this, "Something Went Wrong While Loading OpenCV", Toast.LENGTH_SHORT).show();
+
     }
 }
--- Python/work2.py
+++ Python/work2.py
@@ -4,14 +4,13 @@ assert tf.__version__.startswith('2')
 
 from tensorflow_examples.lite.model_maker.core.data_util.image_dataloader import ImageClassifierDataLoader
 from tensorflow_examples.lite.model_maker.core.task import image_classifier
-from tensorflow_examples.lite.model_maker.core.task.model_spec import mobilenet_v2_spec, resnet_50_spec
+from tensorflow_examples.lite.model_maker.core.task.model_spec import mobilenet_v2_spec, resnet_50_spec, efficientnet_lite0_spec
 
 train_data = ImageClassifierDataLoader.from_folder("/home/mouaz/PycharmProjects/Bitirme/dataset-jpeg")
 test_data =  ImageClassifierDataLoader.from_folder("/home/mouaz/PycharmProjects/Bitirme/dataset-test-jpeg")
 
-# model = image_classifier.create(train_data, batch_size=35, model_spec=resnet_50_spec, epochs=5)
-model = image_classifier.create(train_data, batch_size=35, model_spec=mobilenet_v2_spec, epochs=5)
+model = image_classifier.create(train_data, batch_size=32, model_spec=mobilenet_v2_spec, epochs=10)
 
 loss, accuracy = model.evaluate(test_data)
 
-model.export(export_dir='/home/mouaz/PycharmProjects/Bitirme/Python/LiteModel', with_metadata=True)
+model.export(export_dir='/home/mouaz/PycharmProjects/Bitirme/Android/app/src/main/assets', with_metadata=True)
